735ded1ef1a0d373eefa113c4f036040a4064291
praveen-ofl <praveenkanna.github@gmail.com>
2026-07-31 12:50:57 +0530

    feat(home): remove View All Goals card from journey row

--- frontend/lib/screens/regular_mode/home_screen.dart
+++ frontend/lib/screens/regular_mode/home_screen.dart
@@ -1054,61 +1054,10 @@ class _HomeScreenState extends State<HomeScreen> {
                 icon: Icons.rocket_launch_rounded,
                 categoryName: 'Business & MSME',
               ),
-              _buildViewAllGoalCard(context, provider),
             ],
           ),
         ),
       ],
-    );
-  }
-
-  Widget _buildViewAllGoalCard(BuildContext context, AppProvider provider) {
-    return GestureDetector(
-      onTap: () => provider.updateTabIndex(2),
-      child: Container(
-        width: 86,
-        height: 104,
-        margin: const EdgeInsets.only(right: 12),
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 12),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(16),
-          border: Border.all(color: const Color(0xFFE2E8F0)),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withValues(alpha: 0.015),
-              blurRadius: 6,
-              offset: const Offset(0, 2),
-            ),
-          ],
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              padding: const EdgeInsets.all(6),
-              decoration: const BoxDecoration(
-                color: Color(0xFF2563EB),
-                shape: BoxShape.circle,
-              ),
-              child: const Icon(
-                Icons.arrow_forward_rounded,
-                color: Colors.white,
-                size: 16,
-              ),
-            ),
-            Text(
-              "View All\nGoals",
-              textAlign: TextAlign.center,
-              style: GoogleFonts.inter(
-                fontSize: 10.5,
-                fontWeight: FontWeight.bold,
-                color: const Color(0xFF2563EB),
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 
