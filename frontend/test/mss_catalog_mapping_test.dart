f3c0e0c1a0559adb9e2e1cc3f1aafc5c16fea926
praveen-ofl <praveenkanna.github@gmail.com>
2026-08-06 10:04:25 +0530

    cleanup: ignore avoid_print in mapping test diagnostics

--- frontend/test/mss_catalog_mapping_test.dart
+++ frontend/test/mss_catalog_mapping_test.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: avoid_print
 import 'package:flutter_test/flutter_test.dart';
 import 'package:frontend/services/mss_catalog_bundle.dart';
 import 'package:frontend/services/mss_scheme_adapter.dart';
